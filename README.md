600aee40d2308592657cca0f584080c1aa93e236
GitHub <rakshitbharat@gmail.com>
2025-02-15 17:00:23 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# OBD Raw Data Parser
+
+A lightweight TypeScript library for parsing OBD-II raw data into human readable format. This library helps you interpret raw OBD-II data from your vehicle's diagnostic system.
+
+## Installation
+
---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts'
+  ]
+};
