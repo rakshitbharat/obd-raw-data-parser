7a3653683f76c13b9b00326f103bd25b616f7f52
GitHub <rakshitbharat@gmail.com>
2025-03-06 23:57:17 +0530

    refactor: remove obsolete OBD and DTC related files and update dependencies

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { IObdPIDDescriptor, IParsedOBDResponse } from "./obdTypes.js";
-export declare function parseOBDResponse(hexString: string): IParsedOBDResponse;
-export declare function getPIDInfo(pid: string): IObdPIDDescriptor | null;
-export declare function getAllPIDs(): IObdPIDDescriptor[];
-export { DTCBaseDecoder } from "./DTC/DTCBaseDecoder.js";
-export { VinDecoder } from "./VIN/VinDecoder.js";
